6cecaa839e4571507b9aef9693416e9d8c0bc837
TamizhKarthiKeyan <139999515+TamizhKarthiKeyan@users.noreply.github.com>
2025-05-18 10:28:50 +0530

    first commit

---
+++ frontend/src/app/auth/page.tsx
@@ -0,0 +1,12 @@
+import Login from "@/components/login/Login";
+import React from "react";
+
+const Auth = () => {
+  return (
+    <div>
+      <Login />
+    </div>
+  );
+};
+
+export default Auth;
